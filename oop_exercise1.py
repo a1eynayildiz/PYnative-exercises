7097d5e39dc92603559ca7b53dcfe035d25e2e67
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-08-31 18:59:03 +0300

    designed

---
+++ oop_exercise1.py
@@ -0,0 +1,7 @@
+#Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
+class Vehicle:
+    def __init__(self,max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage=mileage
+car = Vehicle(10,20) 
+print(f"Max Speed: {car.max_speed}, Mileage: {car.mileage}")   
