d5aaa7da02793c12788136c5fab4db515c297ae4
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-09-01 22:24:17 +0300

    overriding

---
+++ oop_exercise10.py
@@ -0,0 +1,29 @@
+#Calculate the area of different shapes using OOP
+"""
+You have given a Shape class and subclasses Circle  and Square. The parent class (Shape) has a area() method.
+
+Now, Write a OOP code to calculate the area of each shapes (each subclass must write its own implementation of area() method to calculates its area).
+"""
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError("Area method must be implemented by subclasses") #raise komutu Python'da hata fırlatmak için kullanılır.
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):  
+        return math.pi * self.radius**2
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):  
+        return self.side * self.side
+
+shapes = [Circle(5), Square(7), Circle(3)]
+
+for shape in shapes:
+    print(shape.area())  
